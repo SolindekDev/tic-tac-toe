aadc9c946865f6e7be1da07bf0af32adb7e726bf
Solindek Developer <83218819+SolindekDev@users.noreply.github.com>
2022-01-08 16:02:12 +0100

    Moving

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 #[warn(unused_variables)]
 #[warn(unused_mut)]
 
@@ -5,12 +7,71 @@ const PLAYER: char = 'X';
 const COMPUTER: char = 'O';
 const TIE: char = ' ';
 
-pub fn free_spaces(table: &mut [[char; 3]; 3]) -> i8 {
-    let free_space: i8 = 9;
+fn get_y() -> i32 {
+    let mut y_str = String::new();
 
-    for row in 1..3 {
-        for col in 1..3 {
-            println!("{} {}", row, col);
+    println!("Enter column from 1 to 3: ");
+    io::stdin()
+        .read_line(&mut y_str)
+        .expect("Failed to read line!");
+
+    let mut y: i32 = y_str.trim().parse()
+        .expect("Failed to parse number!");
+        
+    if y > 3 || y < 1 {
+        println!("Number is bigger than 3 or smaller than 1!");
+        y = 0;
+        get_y();
+    }
+
+    return y;
+}
+
+fn get_x() -> i32 {
+    let mut x_str = String::new();
+
+    println!("Enter row from 1 to 3: ");
+    io::stdin()
+        .read_line(&mut x_str)
+        .expect("Failed to read line!");
+
+    let mut x: i32 = x_str.trim().parse()
+        .expect("Failed to parse number!");
+    
+    if x > 3 || x < 1 {
+        println!("Number is bigger than 3 or smaller than 1!");
+        x = 0;
+        get_x();
+    }
+     
+    return x;
+}
+
+pub fn player_move(table: &mut [[char; 3]; 3]) -> &mut [[char; 3]; 3] {
+    let x: i32 = get_x();
+    let y: i32 = get_y();
+
+    let index_x: usize = (x - 1) as usize;
+    let index_y: usize = (y - 1) as usize;
+
+    if table[index_y][index_x] != ' ' {
+        println!("This place is already taken by {}!", table[index_y][index_x]);
+        player_move(table);
+    }
+
+    table[index_y][index_x] = PLAYER;
+
+    return &mut table;
+}
+
+pub fn free_spaces(table: &mut [[char; 3]; 3]) -> i8 {
+    let mut free_space: i8 = 9;
+
+    for rows in 0..table.len() {
+        for cols in 0..table[rows].len() {
+            if table[rows][cols] != ' ' {
+                free_space -= 1;
+            }
         }
     }
 
@@ -40,14 +101,14 @@ pub fn print_table(table: &mut [[char; 3]; 3]) {
 pub fn main() {
     let mut winner: char = ' ';
     let mut table = [
-        [' ', ' ', ' '],
+        ['X', ' ', ' '],
         [' ', ' ', ' '],
         [' ', ' ', ' '],
     ];
 
-    print_table(&mut table);
-
     while winner == ' ' && free_spaces(&mut table) != 0 {
+        print_table(&mut table);
 
+        table = player_move(&mut table);
     }
 }
